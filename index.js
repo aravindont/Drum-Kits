5c106d9896e7d4d8209f0896c0f4113c72117b97
Aravind104 <ontagodiaravind@gmail.com>
2022-12-29 00:13:20 +0530

    add all the code files

---
+++ index.js
@@ -0,0 +1,24 @@
+const kits = ["crash", "kick", "snare", "tom"];
+const containerEl = document.querySelector(".container");
+kits.forEach((kit) => {
+  const btnEl = document.createElement("button");
+  btnEl.classList.add("btn");
+  containerEl.appendChild(btnEl);
+  btnEl.innerText = kit;
+  btnEl.style.backgroundImage = `url(images/${kit}.png)`;
+  const audioEl = document.createElement("audio");
+  audioEl.src = `sounds/${kit}.mp3`;
+  containerEl.appendChild(audioEl);
+  btnEl.addEventListener("click", () => {
+    audioEl.play();
+  });
+  window.addEventListener("keydown", (e) => {
+    if (e.key === kit.slice(0, 1)) {
+      audioEl.play();
+      btnEl.style.transform = "scale(0.9)";
+      setInterval(() => {
+        btnEl.style.transform = "scale(1)";
+      }, 100);
+    }
+  });
+});
